6f460c7c28147f666fe9f1850b61e2661c104b37
ricardo-henrique <ricardohenrique2013.1@gmail.com>
2025-04-16 11:11:55 -0300

    função sortear criada

---
+++ sorteador-numeros/app.js
@@ -0,0 +1,9 @@
+function sortear() {
+  let quantidade = document.querySelector('#quantidade').value;
+  let de = document.querySelector('#de').value;
+  let ate = document.querySelector('#ate').value;
+
+  alert(`Quantidade: ${quantidade}`);
+  alert(`Do numero: ${de}`);
+  alert(`Até o numero: ${ate}`);
+}
