d02ff2b21ae8700c8de5dc2ae23d610d0bcb18af
ricardo-henrique <ricardohenrique2013.1@gmail.com>
2025-05-01 20:42:00 -0300

    Revert "Logica de Programação - Desafios"
    This reverts commit 435415925a7268f12e9bff4ea4dc1cc9752b3a5b.

--- sorteador-numeros/app.js
+++
@@ -1,9 +0,0 @@
-function sortear() {
-  let quantidade = document.querySelector('#quantidade').value;
-  let de = document.querySelector('#de').value;
-  let ate = document.querySelector('#ate').value;
-
-  alert(`Quantidade: ${quantidade}`);
-  alert(`Do numero: ${de}`);
-  alert(`Até o numero: ${ate}`);
-}
